0f84e52ab95a5b99c43597263d874533c69255bf
ukrWily <ukrwily@gmail.com>
2024-09-04 14:01:32 +0300

    L reducer

---
+++ reducer.js
@@ -0,0 +1,35 @@
+const initialState = [];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+  //
+  // if (action.type === "ADD_NAME") {
+  //   return [...state, action.payload];
+  // }
+  // if (action.type === "DELETE_NAME") {
+  //   return state.filter((personName) => personName !== action.payload);
+  // }
+  // if (action.type === "CLEAR_NAMES") {
+  //   return [];
+  // }
+  // return state;
+}
+
+let newState = reducer(initialState, { type: "ADD_NAME", payload: "Vitalii" });
+console.log(newState);
+newState = reducer(newState, { type: "ADD_NAME", payload: "Wily" });
+console.log(newState);
+newState = reducer(newState, { type: "DELETE_NAME", payload: "Wily" });
+console.log(newState);
+newState = reducer(newState, { type: "CLEAR_NAMES" });
+console.log(newState);
+newState = reducer(initialState, { type: "ADD_NAME", payload: "Vitalii" });
+console.log(newState);
+newState = reducer(newState, { type: "ADD_NAME", payload: "Wily" });
+console.log(newState);
